64857f410f4ccdfbbd4ead82beb905a4ac9ee352
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2024-07-02 12:57:02 -0400

    Add description (string) column to Product Entity.

--- src/main/java/com/example/springproject/Product.java
+++ src/main/java/com/example/springproject/Product.java
@@ -15,36 +15,37 @@ public class Product {
     private Long quantity;
     @Column
     private double price;
+    @Column
+    private String description;
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public Long getQuantity() {
         return quantity;
     }
-
     public void setQuantity(Long quantity) {
         this.quantity = quantity;
     }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
